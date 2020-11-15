4284487971ea2fc192629b7c4497f582aa007ee8
willard-yuan <yongyuanstu@gmail.com>
2020-11-15 23:49:25 +0800

    fasttext embedding

---
+++ fasttext_embedding/fasttext_model.cc
@@ -0,0 +1,39 @@
+#include "fasttext_model.h"
+
+namespace cvtk {
+namespace nlp {
+bool FasttextModel::Init(const std::string& path) {
+  fasttext_.loadModel(path);
+  return true;
+}
+void FasttextModel::GetEmbedd(const std::string &query,
+                                std::vector<float>* features) {
+  fasttext::Vector vec(fasttext_.getDimension());
+  fasttext_.getWordVector(vec, query);
+  for (int j = 0; j < vec.size(); ++j) {
+    features[j] = vec[j];
+  }
+  L2Norm(features, vec.size());
+}
+void FasttextModel::GetEmbedd(const std::string &query, float* features) {
+  fasttext::Vector vec(fasttext_.getDimension());
+  fasttext_.getWordVector(vec, query);
+  for (int j = 0; j < vec.size(); ++j) {
+    features[j] = vec[j];
+  }
+  L2Norm(features, vec.size());
+}
+
+void FasttextModel::L2Norm(float* vector, int d) {
+  double accum = 0.0;
+  for (int i = 0; i < d; ++i) {
+    accum += vector[i] * vector[i];
+  }
+   accum = sqrt(accum);
+   float denorm_v = std::max((double)1e-12, (double)accum);
+   for (int i = 0; i < d; ++i) {
+     vector[i] = vector[i]/denorm_v;
+   }
+ }
+}  // namespace search
+}  // namespace mmu
---
+++ fasttext_embedding/fasttext_model.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+#include "./third_party/fasttext/fasttext.h"
+
+namespace cvtk {
+namespace nlp {
+class FasttextModel {
+ public:
+  bool Init(const std::string& path);
+  void GetEmbedd(const std::string &query, float* features);
+  void GetEmbedd(const std::string &query, std::vector<float>* features);
+  void NormL2(const std::vector<float>& vecs,  std::vector<float>* norm);
+ private:
+  fasttext::FastText fasttext_;
+};
+
+}  // namespace nlp
+}  // namespace cvtk
