bd05b4086903296361f4e7c2bf3719940b9e11e6
willard-yuan <yongyuanstu@gmail.com>
2020-12-10 01:02:09 +0800

    data_convert_bin update to python3.7+

--- int8_quant/train/data_convert_bin.py
+++ int8_quant/train/data_convert_bin.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# python2.7
+# python3.7
 
 import struct
 import random
@@ -8,7 +8,7 @@ from sklearn.preprocessing import normalize as sknormalize
 
 
 dim_feat = 64
-qfeats_path = '/media/cephfs4/yuanyong/td-dz-g599/cross_modal_retrieval/10_test.txt'
+qfeats_path = 'embed_v2.txt'
 
 qfeats_file = open(qfeats_path, 'r')
 qcontent = qfeats_file.readlines()
@@ -28,8 +28,8 @@ count = 0
 
 # 写入查询文件
 for i, line in enumerate(qcontent):
-    feat = [float(value) for value in line.strip().split('\x01')[1].split('\x02')]
-    id_ = line.strip().split('\x01')[0]
+    feat = [float(value) for value in line.strip().split(',')[1].split('\x02')]
+    id_ = line.strip().split(',')[0]
     if(len(feat) != dim_feat):
         print("error: %s" % id_)
         continue
@@ -46,17 +46,27 @@ del qcontent
 featsArray = normalize(featsArray)
 
 # 库特征写入二进制
-db_bin_file = open('/media/cephfs4/yuanyong/td-dz-g599/cross_modal_retrieval/10_test.bin', 'wb')
+db_bin_file = open('embed_v2.bin', 'wb')
 line_binary = struct.pack('i', count)
 db_bin_file.write(line_binary)
 for i in range(count):
     tmpFeat = list(featsArray[i,:])
     tmpId = ids[i]
-    format_ = 'i' + 's'*len(tmpId)
-    line_binary = struct.pack(format_, len(tmpId), *tmpId)
+    #format_ = 'i' + 's'*len(tmpId)
+    #line_binary = struct.pack(format_, len(tmpId), *tmpId)
+    line_binary = struct.pack('i', len(tmpId))
     db_bin_file.write(line_binary)
-    format_ = 'i' + 'f'*dim_feat
-    line_binary = struct.pack(format_, dim_feat, *tmpFeat)
+    for str_ in tmpId:
+        line_binary = struct.pack('s', bytes(tmpId.encode('utf-8')))
+        db_bin_file.write(line_binary)
+    #line_binary = struct.pack(format_, len(tmpId), *tmpId),
+    #db_bin_file.write(line_binary)
+    #format_ = 'i' + 'f'*dim_feat
+    #line_binary = struct.pack(format_, dim_feat, *tmpFeat)
+    #db_bin_file.write(line_binary)
+    line_binary = struct.pack('i', dim_feat)
+    db_bin_file.write(line_binary)
+    line_binary = struct.pack('f'*dim_feat, *tmpFeat)
     db_bin_file.write(line_binary)
 db_bin_file.close()
 print("database finished writing to binary file")
