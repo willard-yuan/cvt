9fc25037da90a82a934e1df156a0a5a48cb3eb3f
Yong Yuan <yongyuanstu@gmail.com>
2021-10-21 23:50:50 +0800

    Update canny_edge_detector.h

--- pip_detection/pip_detection_algo/include/canny_edge_detector.h
+++ pip_detection/pip_detection_algo/include/canny_edge_detector.h
@@ -4,19 +4,16 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgproc/types_c.h"
-namespace mmu {
 
+namespace cvtk {
 class CannyEdgeDetector {
-
 public:
  CannyEdgeDetector();
     cv::Mat detectEdges(cv::Mat & input_frame, cv::Mat & canny_edge_detecion_frame);
-
 private:
     int low_threshold_max;
     int high_threshold_max;
     int low_slider_position;
     int high_slider_position;
 };
-
-} // end namespce mmu
+} // end namespce cvtk
